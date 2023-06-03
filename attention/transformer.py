6ac6739973919f4b2caa96daffc3dc4ddf57a769
sovit-123 <sovitrath5@gmail.com>
2023-06-03 21:04:25 +0530

    Positional embedding shape correction

--- attention/transformer.py
+++ attention/transformer.py
@@ -23,7 +23,7 @@ class PositionalEncoding(nn.Module):
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0, 1)
+        pe = pe.unsqueeze(0)
         self.register_buffer('pe', pe)
 
     def forward(self, x):
@@ -35,7 +35,7 @@ class PositionalEncoding(nn.Module):
             output: [sequence length, batch size, embed dim]
         """
 
-        x = x + self.pe[:x.size(0), :]
+        x = x + self.pe[:, :x.size(1)]
         return self.dropout(x)
     
 class Embedding(nn.Module):
