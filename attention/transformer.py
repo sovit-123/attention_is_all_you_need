566b8cbde00fd948c716d9f1804d81fa4e259d15
sovit-123 <sovitrath5@gmail.com>
2023-06-06 08:00:08 +0530

    Getting results for language translation training now

--- attention/transformer.py
+++ attention/transformer.py
@@ -331,17 +331,21 @@ class Transformer(nn.Module):
     def make_tgt_mask(self, tgt, pad_token_id=1):
         """
         :param tgt: Target sequence.
+        :param pad_token_id: Padding token ID, default 1.
         Returns:
             tgt_mask: Target mask.
         """
         batch_size = tgt.shape[0]
         device = tgt.device
-
+        # Some help from here:
+        # https://github.com/gordicaleksa/pytorch-original-transformer/blob/main/utils/data_utils.py
         # Same as src_mask but we additionally want to mask tokens from looking forward into the future tokens
         # Note: wherever the mask value is true we want to attend to that token, otherwise we mask (ignore) it.
         sequence_length = tgt.shape[1]  # trg_token_ids shape = (B, T) where T max trg token-sequence length
         trg_padding_mask = (tgt != pad_token_id).view(batch_size, 1, 1, -1)  # shape = (B, 1, 1, T)
-        trg_no_look_forward_mask = torch.triu(torch.ones((1, 1, sequence_length, sequence_length), device=device) == 1).transpose(2, 3)
+        trg_no_look_forward_mask = torch.triu(torch.ones((
+            1, 1, sequence_length, sequence_length), device=device
+        ) == 1).transpose(2, 3)
 
         # logic AND operation (both padding mask and no-look-forward must be true to attend to a certain target token)
         tgt_mask = trg_padding_mask & trg_no_look_forward_mask  # final shape = (B, 1, T, T)
@@ -350,12 +354,13 @@ class Transformer(nn.Module):
     def make_src_mask(self, src, pad_token_id=1):
         """
         :param src: Source sequence.
-
+        :param pad_token_id: Padding token ID, default 1.
         Returns:
             src_mask: Source mask.
         """
         batch_size = src.shape[0]
-
+        # Some help from here:
+        # https://github.com/gordicaleksa/pytorch-original-transformer/blob/main/utils/data_utils.py
         # src_mask shape = (B, 1, 1, S) check out attention function in transformer_model.py where masks are applied
         # src_mask only masks pad tokens as we want to ignore their representations (no information in there...)
         src_mask = (src != pad_token_id).view(batch_size, 1, 1, -1)
